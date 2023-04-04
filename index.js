1cab8bcd05afa9fedb23abe23aaab5b5756b03ea
jeremykiokoo <jeremy.kioko@student.moringaschool.com>
2023-04-04 14:49:01 +0300

    codeComplete

---
+++ index.js
@@ -0,0 +1,24 @@
+let fetchedData;
+fetch('http://localhost:3000/Movies')
+.then((result) => result.json())
+.then((result2) => {
+ fetchedData = result2;
+ console.log('results => ', result2);
+//displaying data 
+document.getElementById('root').innerHTML=data.card
+let card = ""
+data.forEach((Movies)=>{
+console.log(Movies)
+card +=  `<div class="banner-card">
+<img src=${Movies.image} alt="https://i.ytimg.com/vi/32RAq6JzY-w/maxresdefault.jpg">
+<div class="banner-text">
+    <h4>${Movies.name}</h4>
+    <p>${Movies.about}</p>
+    <p>${Movies.likes}</p>
+</div>`})
+console.log(card)
+document.querySelector('.banner-card').innerHTML=card
+console.log(data);  
+})
+//catching any errors that may occur
+.catch(error => console.error(error));
